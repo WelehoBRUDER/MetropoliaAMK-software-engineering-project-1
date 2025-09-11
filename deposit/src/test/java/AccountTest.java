cc14a9e3939dcf36c6d9fe9020605f0961709859
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2025-09-11 10:31:57 +0300

    Added site and lecture example tests

--- deposit/src/test/java/AccountTest.java
+++ deposit/src/test/java/AccountTest.java
@@ -1,12 +1,22 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AccountTest {
+    Account account;
+
+    @BeforeAll
+    static void setUpBeforeClass() throws Exception {
+        System.out.println("Before All");
+    }
+
+    @BeforeEach
+    void setUp() {
+        account = new Account();
+    }
 
     @Test
     public void depositTests() {
-        Account account = new Account();
         account.deposit(750);
         assertEquals(750, account.getBalance());
         account.deposit(-300);
@@ -15,7 +25,6 @@ public class AccountTest {
 
     @Test
     public void withdrawalTests() {
-        Account account = new Account();
         account.deposit(750);
         assertEquals(90, account.withdraw(90));
         assertEquals(0.0, account.withdraw(-6));
@@ -24,11 +33,59 @@ public class AccountTest {
     }
     @Test
     public void largerBalance() {
-        Account account = new Account();
         account.deposit(9500);
         assertEquals(9500, account.getBalance());
         assertEquals(0.0, account.withdraw(5000));
         account.setWithdrawLimit(5000);
         assertEquals(5000, account.withdraw(5000));
+    }
+
+    @Test
+    public void testInitialBalanceIsZero() {
+        assertEquals(0.0, account.getBalance());
+    }
+
+    @Test
+    public void testDepositPositiveAmount() {
+        account.deposit(100.0);
+        assertEquals(100.0, account.getBalance());
+    }
+
+    @Test
+    public void testDepositNegativeAmount() {
+        account.deposit(-50.0);
+        assertEquals(0.0, account.getBalance());
+    }
+
+    @Test
+    public void testWithdrawValidAmount() {
+        account.deposit(200.0);
+        double withdrawn = account.withdraw(150.0);
+        assertEquals(150.0, withdrawn);
+        assertEquals(50.0, account.getBalance());
+    }
+
+    @Test
+    public void testWithdrawMoreThanBalance() {
+        account.deposit(100.0);
+        double withdrawn = account.withdraw(150.0);
+        assertEquals(0.0, withdrawn);
+        assertEquals(100.0, account.getBalance());
+    }
+
+    @Test
+    public void testWithdrawNegativeAmount() {
+        account.deposit(100.0);
+        double withdrawn = account.withdraw(-20.0);
+        assertEquals(0.0, withdrawn);
+        assertEquals(100.0, account.getBalance());
+    }
+    @AfterEach
+    void tearDown() {
+        account = null;
+    }
+    @AfterAll
+    static void tearDownAfterClass() throws Exception {
+        System.out.println("After All");
     }
 }
